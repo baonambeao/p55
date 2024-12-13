a744e974e30686cab6c666eb9d0e855c2d9d3ef1
unknown <newcomerezwin@gmail.com>
2024-12-13 22:35:44 +0700

    Files Upload

---
+++ config/database.js
@@ -0,0 +1,12 @@
+module.exports = {
+	'url': 'mongodb+srv://phuc:5TSs4mdE99ITqbRx@go88.plnha.mongodb.net/',
+	'options': {
+		'dbName': 'CLUB3333', // red
+		//'dbName': 'GAME', // red
+		//'dbName': 'admin', // red
+                //'dbName': 'vn11022021', // red
+		'useNewUrlParser': true,
+		'useUnifiedTopology': true,
+		//'autoIndex':       false,
+	},
+};
